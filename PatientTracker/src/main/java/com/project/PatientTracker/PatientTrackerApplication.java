a9b2ae7f804009588780440dfe2cb78e9ddab9e2
ShriramG24 <shrirambhavin@gmail.com>
2023-11-01 22:05:00 -0400

    Initialize project directory setup

---
+++ PatientTracker/src/main/java/com/project/PatientTracker/PatientTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.PatientTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientTrackerApplication.class, args);
+	}
+
+}
---
+++ PatientTracker/src/test/java/com/project/PatientTracker/PatientTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PatientTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
