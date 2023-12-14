9cdadcb910222b9a32743cea59c84c0817028079
SchrOdinger11 <sudhanshu.kulkarni2105@gmail.com>
2023-12-14 15:23:56 -0500

    dashboard initiated

--- PatientTracker/src/main/Frontend/src/app/app-routing.module.ts
+++ PatientTracker/src/main/Frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AppointmentsComponent } from './components/appointments/appointments.component';
 import { FindDoctorComponent } from './components/find-doctor/find-doctor.component';
 import { BookappointmentComponent } from './components/bookappointment/bookappointment.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
     component: AppointmentsComponent,
   },
   {
+<<<<<<< Updated upstream
     path: 'find-doctor',
     component: FindDoctorComponent,
   },
@@ -21,6 +23,11 @@ const routes: Routes = [
     path: 'book-appointment',
     component: BookappointmentComponent,
   },
+=======
+    path:'dashboard',
+    component:DashboardComponent,
+  }
+>>>>>>> Stashed changes
 ];
 
 @NgModule({
