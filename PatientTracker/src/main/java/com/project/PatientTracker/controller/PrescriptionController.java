963ff748cf8899b3f3dd770c058888f663d69cb9
ShriramG24 <shrirambhavin@gmail.com>
2023-12-14 18:00:09 -0500

    Add controller for prescriptions and tests

---
+++ PatientTracker/src/main/java/com/project/PatientTracker/controller/PrescriptionController.java
@@ -0,0 +1,48 @@
+package com.project.PatientTracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.project.PatientTracker.model.Prescription;
+import com.project.PatientTracker.repository.PrescriptionRepository;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api/")
+public class PrescriptionController {
+    @Autowired
+    private PrescriptionRepository prescriptionRepository;
+
+    // Get All Prescriptions by Patient
+    @GetMapping("/prescriptions/patients/{patientId}")
+    public ResponseEntity<List<Prescription>> getPrescriptionsByPatient(@PathVariable Long patientId) {
+        List<Prescription> prescriptions = prescriptionRepository.findByPatientId(patientId);
+
+        return ResponseEntity.ok(prescriptions);
+    }
+
+    // Create Prescription
+    @PostMapping("/prescriptions")
+    public ResponseEntity<Prescription> createPrescription(@RequestBody Prescription prescriptionRequest) {
+        Prescription prescription = new Prescription()
+            .setDoctor(prescriptionRequest.getDoctor())
+            .setPatient(prescriptionRequest.getPatient())
+            .setMedicines(prescriptionRequest.getMedicines())
+            .setReports(prescriptionRequest.getReports())
+            .setInstructions(prescriptionRequest.getInstructions());
+
+        return ResponseEntity.ok(prescriptionRepository.save(prescription));
+    }
+
+    // Delete Prescription
+    @DeleteMapping("/prescriptions/patients/{patientId}")
+    public ResponseEntity<Prescription> deletePrescription(@PathVariable Long patientId) {
+        Prescription prescription = prescriptionRepository.findByPatientId(patientId).get(0);
+
+        prescriptionRepository.delete(prescription);
+        return ResponseEntity.ok(prescription);
+    }
+}
