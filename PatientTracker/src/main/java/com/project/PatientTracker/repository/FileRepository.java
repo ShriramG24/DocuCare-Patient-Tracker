b45b737ecf83935ff2b28f4eefdf1e4470ac5fa3
ShriramG24 <shrirambhavin@gmail.com>
2023-12-14 03:00:31 -0500

    Add get methods to FileRepository

--- PatientTracker/src/main/java/com/project/PatientTracker/repository/FileRepository.java
+++ PatientTracker/src/main/java/com/project/PatientTracker/repository/FileRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.PatientTracker.model.File;
+import com.project.PatientTracker.model.Patient;
 import com.project.PatientTracker.model.Doctor;
 
 public interface FileRepository extends JpaRepository<File, Long> {
 
-    public List<File> findByOwner(Doctor doctor);
+    public List<File> findByDoctor(Doctor doctor);
+
+    public List<File> findByPatient(Patient patient);
 }
