46aee799c1a059b044a7c2301b19b8ebddf454b7
ShriramG24 <shrirambhavin@gmail.com>
2023-12-14 20:09:17 -0500

    Define file handling exceptions

---
+++ PatientTracker/src/main/java/com/project/PatientTracker/exception/FileEmptyException.java
@@ -0,0 +1,8 @@
+package com.project.PatientTracker.exception;
+
+public class FileEmptyException extends Exception {
+
+    public FileEmptyException(String message) {
+        super(message);
+    }
+}
