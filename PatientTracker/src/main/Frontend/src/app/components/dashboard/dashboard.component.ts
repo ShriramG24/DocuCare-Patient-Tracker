4e406366606210c9ee63088b225483b2c2c68cf4
dhruvin5 <dhruvingandhi5@gmail.com>
2023-12-15 02:38:52 -0500

    improved dashboard

--- PatientTracker/src/main/Frontend/src/app/components/dashboard/dashboard.component.ts
+++ PatientTracker/src/main/Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GetPatientDoctorsService } from 'app/controller/get-patient-doctors.service';
+import { Patient } from 'app/models/patient.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  patient = new Patient();
+  constructor( private getPatientDoctor: GetPatientDoctorsService ) {
+    
+  }
+  ngOnInit(): void {
+    // Example: Fetch data from the backend
+    this.getPatientDoctor.getPatient(1).subscribe((data) => {
+      console.log(data);
+      this.user = data;
+    }, (error) => {
+      console.error('Error fetching patient data', error);
+    });
+   
+    
+}
 user ={type: "Patient",
-       username:"Allah",
+       firstName:"Allah",
+       lastName:"Allah",
        consultation: [ 1 ],
-      medicine: "0",
+      medicine: ["bp"],
+      tests: ["Blood test"],
+      instructions: ["Avoid Sugar"],
     finalRating:" 0.0",
     numberOfAppointments:"0",
   appointment_dates:[1],
